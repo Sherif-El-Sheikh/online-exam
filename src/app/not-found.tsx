4ce34d531187b4cebf663a4623d4b51686d3bc04
Sherif-El-Sheikh <shreefsheikh99@gmail.com>
2025-04-11 18:26:34 +0200

    Add API folder, error handling components and authentication routes

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+export default function NotFound() {
+    return (
+        <html>
+            <body>
+                <main>
+                    <p>Page not found.</p>
+                </main>
+            </body>
+        </html>
+    )
+}
