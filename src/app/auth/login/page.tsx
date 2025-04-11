483c42460857ddc3ffa1bddfcc8ee5a335d0ffed
Sherif-El-Sheikh <shreefsheikh99@gmail.com>
2025-04-11 19:42:27 +0200

    Add Tailwind CSS class sorting plugin and  Input component

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -2,8 +2,12 @@ import { Input } from "@/components/ui/input";
 
 export default function Page() {
     return (
-        <main>
-        <Input/>
+        <main className="p-4">
+            <Input
+            type="email"
+            placeholder="Enter Email"
+            className="h-12 w-1/2 rounded-md border border-slate-200 px-3"
+            />
         </main>
-    )
+    );
 }
