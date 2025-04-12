195773de3cfce87b5d2b74fe0d5face0cc4790e0
Sherif-El-Sheikh <shreefsheikh99@gmail.com>
2025-04-12 15:02:51 +0200

    Add PasswordInput with toggle visibility

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,9 +1,12 @@
+"use client"
+import { PasswordInput } from "@/components/common/password-input";
 import { VariantInput } from "@/components/common/variant-input";
 
 export default function Page() {
     return (
         <main className="p-4">
-            <VariantInput variant="auth" placeholder="Enter Email"/>
+            <VariantInput variant="auth" placeholder="Enter Email" className="mb-4"/>
+            <PasswordInput placeholder="Password" className=""/>
         </main>
     );
 }
