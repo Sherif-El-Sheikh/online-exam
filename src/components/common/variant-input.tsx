6341db859baff6fb1aa12bbb43d47070fe5306c7
Sherif-El-Sheikh <shreefsheikh99@gmail.com>
2025-04-12 23:24:16 +0200

    add custom button variants

--- src/components/common/variant-input.tsx
+++ src/components/common/variant-input.tsx
@@ -13,13 +13,13 @@ interface VariantInputProps extends React.ComponentProps<typeof Input> {
 
 //Styles for each variant
 const variantStyles: Record<StyledInputVariant, string> = {
-    auth:"h-10 sm:h-12 rounded-lg border border-slate-300 px-3 py-4 text-sm/6 sm:text-base/6 placeholder:text-slate-500 placeholder:text-sm/6 sm:placeholder:text-base/6 focus-visible:ring-2 focus-visible:ring-submain shadow-auth-input bg-slate-50",
+    auth:"sm:w-[410px] h-10 sm:h-12 rounded-lg border border-slate-300 px-3 py-4 text-sm/6 sm:text-base/6 placeholder:text-slate-500 placeholder:text-sm/6 sm:placeholder:text-base/6 focus-visible:ring-2 focus-visible:ring-submain shadow-auth-input bg-slate-50 font-inter font-medium",
     search:
-    "h-12 sm:h-14 rounded-[1.3rem] border-none shadow-search py-4 px-16 text-base/[100%] sm:text-lg/[100%] placeholder:text-base/[100%] sm:placeholder:text-lg/[100%] focus-visible:ring-1 focus-visible:ring-submain",
+    "md:w-[370px] min-[990px]:w-[540px] lg:w-[590px] xl:w-[760px] h-10 sm:h-12 md:h-14 rounded-[1.3rem] border-none shadow-search py-4 px-8 sm:px-16 text-sm/[100%] md:text-base/[100%] lg:text-lg/[100%] placeholder:text-xs/[100%] sm:placeholder:text-sm/[100%] md:placeholder:text-base/[100%] lg:placeholder:text-lg/[100%] focus-visible:ring-1 focus-visible:ring-submain font-poppins font-normal",
     diploma:
-    "h-10 rounded-2xl border border-slate-400 focus-visible:ring-1 focus-visible:border-none focus-visible:ring-main p-3",
+    "w-60 h-10 rounded-2xl border border-slate-400 focus-visible:ring-1 focus-visible:border-none focus-visible:ring-main p-3 font-inter font-semibold",
     question:
-    "h-10 rounded-2xl border border-slate-400 focus-visible:ring-1 focus-visible:border-none focus-visible:ring-main py-3 px-7",
+    "sm:w-[620px] md:w-[673px] h-10 rounded-2xl border border-slate-400 focus-visible:ring-1 focus-visible:border-none focus-visible:ring-main py-3 px-7 font-inter font-semibold",
 };
 
 const VariantInput = React.forwardRef<HTMLInputElement, VariantInputProps>(
@@ -31,7 +31,7 @@ const VariantInput = React.forwardRef<HTMLInputElement, VariantInputProps>(
         <div className="relative">
             {/* Render search icon for 'search' variant */}
             {variant === "search" && (
-            <SearchIcon className="absolute left-4 top-1/2 h-5 w-5 -translate-y-1/2 transform text-blue-700 sm:h-6 sm:w-6" />
+            <SearchIcon className="absolute left-2 sm:left-4 top-1/2 h-4 w-4 sm:h-5 sm:w-5 -translate-y-1/2 transform text-blue-700 lg:h-6 lg:w-6 " />
             )}
 
             {/* Render input with the selected variant */}
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -18,20 +18,32 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        auth: "bg-main w-4/5 sm:w-[410px] hover:bg-submain text-base font-poppins font-normal text-white shadow-primary-auth transition-all duration-300",
+        register:
+          "h-10 w-32 rounded-2xl border border-slate-300 bg-transparent text-main hover:bg-slate-100/70 transition-all duration-300 shadow-auth-input font-inter font-normal text-base sm:text-xl p-4 sm:p-5",
+        startQuiz:
+          "h-10 sm:h-12 md:h-14 w-24 md:w-36 min-[990px]:w-44 xl:w-48 rounded-[20px] bg-main hover:bg-submain transition-all duration-400 sm:py-4 sm:px-8 font-poppins font-semibold text-xs sm:text-sm md:text-base lg:text-lg text-white",
+        start:
+          "h-6 w-20 rounded-xl py-3 px-6 bg-main hover:bg-submain font-inter font-medium text-sm transition-all duration-300 text-white",
+        addQuestion:
+          "h-6 w-32 rounded-xl py-3 px-6 bg-main hover:bg-submain font-inter font-medium text-sm transition-all duration-300 text-white",
+        back: "h-12 w-72 bg-transparent hover:bg-transparent border text-main border-main py-3 px-6 rounded-3xl font-roboto font-medium text-base sm:text-xl",
+        next: "h-12 w-72 bg-main hover:bg-submain text-white py-3 px-6 rounded-3xl transition-all duration-300 font-roboto font-medium text-base sm:text-xl",
+        add: "h-8 w-48 rounded-xl py-2 px-14 bg-main hover:bg-submain text-white transition-all duration-300 font-inter font-bold sm:text-base ",
       },
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
+        xl: "h-12 sm:h-14 rounded-[20px] p-2",
         icon: "h-10 w-10",
       },
     },
     defaultVariants: {
       variant: "default",
-      size: "default",
     },
-  }
-)
+  },
+);
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
