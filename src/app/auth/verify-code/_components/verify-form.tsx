0dc1e17e313a87828edccd4d09b6e896f8babd0c
Sherif-El-Sheikh <shreefsheikh99@gmail.com>
2025-04-15 10:23:20 +0200

    create VerifyForm component and update verify code page

--- src/app/auth/verify-code/_components/verify-form.tsx
+++ src/app/auth/verify-code/_components/verify-form.tsx
@@ -10,33 +10,33 @@ import {
 } from "@/components/ui/form";
 import { VariantInput } from "@/components/common/variant-input";
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
 
-export default function ForgotForm() {
+
+export default function VerifyForm() {
     const form = useForm({
         defaultValues: {
-        email: "",
+        resetCode: "",
         },
     });
     return (
         <Form {...form}>
             <form className="min-[576px]:max-md:mx-auto min-[576px]:max-md:w-3/4 md:w-4/5 lg:w-3/4 xl:w-4/6 2xl:w-1/2">
-                {/* Forgot */}
+                {/* Verify Code */}
                 <FormField
                 control={form.control}
-                name="email"
+                name="resetCode"
                 // eslint-disable-next-line @typescript-eslint/no-unused-vars
                 render={({ field }) => (
                     <FormItem className="mb-8">
                         {/* Label */}
-                        <FormLabel className="sr-only">Email</FormLabel>
+                        <FormLabel className="sr-only">Verify Code</FormLabel>
 
-                        {/* Forgot Input */}
+                        {/* Verify Code Input */}
                         <FormControl>
                             <VariantInput
                             variant="auth"
-                            type="email"
-                            placeholder="Enter Email"
+                            type="text"
+                            placeholder="Enter Code"
                             />
                         </FormControl>
 
@@ -46,20 +46,18 @@ export default function ForgotForm() {
                 )}
                 />
 
-                {/* Send Mail */}
-                <Button size="xl" variant="auth" type="submit" className="mb-6 sm:mb-8">
-                Send Mail
-                </Button>
-
-                {/* Remembered your password */}
-                <p className="my-4 text-center font-poppins text-sm font-normal lg:text-base">
-                Remembered your password?
-                    <Link href="/auth/login" className="mx-2 text-main">
-                        Sign in
-                    </Link>
+                {/* Have an account */}
+                <p className="mt-4 mb-6 text-end font-poppins text-sm font-normal lg:text-base">
+                Didn’t receive a code?
+                    <Button variant="ghost" className="mx-2 text-main hover:text-main hover:bg-transparent">
+                        Resend
+                    </Button>
                 </p>
 
-
+                {/* Send Mail */}
+                <Button size="xl" variant="auth" type="submit">
+                Verify
+                </Button>
             </form>
         </Form>
     )
--- src/app/auth/verify-code/page.tsx
+++ src/app/auth/verify-code/page.tsx
@@ -1,7 +1,15 @@
+import VerifyForm from "./_components/verify-form";
+
 export default function Page() {
     return (
-        <main>
-        verify code
-        </main>
+        <div className="px-3 py-2 md:mt-20 md:max-lg:px-0 2xl:mt-28">
+            {/* Title */}
+            <h2 className="mb-3 font-inter text-base font-bold min-[576px]:max-md:ml-20 sm:text-lg md:mb-5 md:text-xl lg:text-2xl">
+            Verify code
+            </h2>
+
+            {/* Verify Form */}
+            <VerifyForm/>
+        </div>
     )
 }
