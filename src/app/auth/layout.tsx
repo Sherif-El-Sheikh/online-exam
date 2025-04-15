f648c4da721f385435f5aa033170fcf801fa5b52
Sherif-El-Sheikh <shreefsheikh99@gmail.com>
2025-04-15 21:29:12 +0200

    Enhance responsive design

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -4,7 +4,7 @@ import AuthSide from "@/components/features/auth/auth-side";
 
 export default function AuthLayout({children}: {children: React.ReactNode}) {
     return (
-        <main className="min-h-screen md:flex md:justify-between md:gap-16 lg:gap-20 xl:gap-24 min-[1300px]:gap-32 2xl:gap-72">
+        <main className="min-h-screen md:flex md:justify-between md:gap-16 lg:gap-20 xl:gap-24 min-[1300px]:gap-32 3xl:gap-72">
             {/* Auth Side */}
             <AuthSide />
 
--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from "./_components/login-form";
 
 export default function Page() {
     return (
-        <div className="px-3 py-2 md:max-lg:px-0 md:mt-20 2xl:mt-28">
+        <div className="px-3 py-2 md:max-lg:px-0 md:mt-20 3xl:mt-28">
             {/* Title */}
             <h2 className="mb-3 font-inter text-base font-bold min-[576px]:max-md:ml-20 sm:text-lg md:mb-5 md:text-xl lg:text-2xl">
                 Sign in
