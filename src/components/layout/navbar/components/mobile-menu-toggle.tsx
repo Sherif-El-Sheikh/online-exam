f05ffa639e057acf1ebb9c5a9ba5dd74a83096b5
Sherif-El-Sheikh <shreefsheikh99@gmail.com>
2025-04-20 19:00:31 +0200

    Fix forward ref to Button in MobileMenuToggle

--- src/components/layout/navbar/components/mobile-menu-toggle.tsx
+++ src/components/layout/navbar/components/mobile-menu-toggle.tsx
@@ -2,13 +2,17 @@
 import { useSidebar } from "@/components/providers/sidebar/sidebar.provider"
 import { Button } from "@/components/ui/button"
 import { Menu } from "lucide-react"
+import React from "react"
 
-export const MobileMenuToggle = () => {
+// React.forwardRef to forward the ref from parent to child
+export const MobileMenuToggle = React.forwardRef<HTMLButtonElement> ((_, ref) => {
     // Accessing sidebar states using useSidebar hook
     const {isMobile, toggleMobileSidebar} = useSidebar();
-    
+
     return (
         <Button
+        // Allowing the parent to access the DOM element directly
+        ref={ref}
         variant="ghost"
         size="icon"
         onClick={toggleMobileSidebar}
@@ -17,4 +21,7 @@ export const MobileMenuToggle = () => {
             <Menu/>
         </Button>
     )
-}
+})
+
+// displayName to "MobileMenuToggle" for the component to appear with this name in the DOM
+MobileMenuToggle.displayName = "MobileMenuToggle"
